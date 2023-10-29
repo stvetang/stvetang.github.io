c8d8141c424b26561a811f5be5c38d64d3fb8719
Steve Tang <132315005+stvetang@users.noreply.github.com>
2023-10-29 16:52:38 -0700

    Add files via upload

---
+++ practice-one.js
@@ -0,0 +1,23 @@
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+
+let exampleObject = {
+    id: 1,
+    name: "Steve Tang"
+}
+
+console.log(exampleObject);
+console.log("My name is: ",exampleObject.name);
+
+function doSomething(){
+    let functionVar = 10;
+    for(let i =0; i <10; i++){
+        console.log(i);
+    }
+}
+
+doSomething();
